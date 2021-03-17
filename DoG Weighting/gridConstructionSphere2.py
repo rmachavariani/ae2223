4160eb7971ae81b4b4edd1963157f5cbf853373c
Rosa <email@gmail.com>
2021-03-17 17:19:05 +0100

    Limited the data to 500 rows

--- DoG Weighting/gridConstructionSphere2.py
+++ DoG Weighting/gridConstructionSphere2.py
@@ -10,7 +10,7 @@ def loadData():
     '''
     # load the data
     t1 = time.time()
-    data = np.loadtxt("carMirrorData.dat", max_rows = 500)
+    data = np.loadtxt("carMirrorData.dat", max_rows=500)
     t2 = time.time()
     print("Loading done in ", "{:.2f}".format(t2 - t1), " s")
 
