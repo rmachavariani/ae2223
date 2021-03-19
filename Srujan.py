2942867e18bfdf72d6847ec2fd412bb893ca1f67
clockserious <69359663+clockserious@users.noreply.github.com>
2021-03-19 14:26:14 +0100

    Get particle data in the bin

--- Srujan.py
+++ Srujan.py
@@ -1,5 +1,5 @@
 from Andreas.gridConstruction import *
-import pdb
+
 
 def fetch_vector(test_bin):
     particles_cord = np.empty((len(test_bin), 3))
@@ -17,20 +17,21 @@ def fetch_vector(test_bin):
 
             particles_cord[i] = [x, y, z]
             particles_vel[i] = [u, v, w]
-    pdb.set_trace()
+
+
     return particles_cord, particles_vel
 
 
-def basis(particles_cord, cell, particles_vel):
+def basis(particles_cord):
     design_matrix = np.empty((len(particles_cord), 10))
     for i in range(len(particles_cord)):
-        dx = abs(particles_cord[i, 0] - cell.x)
-        dy = abs(particles_cord[i, 1] - cell.y)
-        dz = abs(particles_cord[i, 2] - cell.z)
+        dx = particles_cord[i, 0]
+        dy = particles_cord[i, 1]
+        dz = particles_cord[i, 2]
 
         design_matrix[i] = [1, dx, dy, dz, dx*dy, dx*dz, dy*dz, dx**2, dy**2, dz**2]
-
-    return np.dot(design_matrix, np.transpose(design_matrix)), np.dot(np.transpose(design_matrix), np.transpose(particles_vel[:, 0]))
+    print(design_matrix)
+    return design_matrix
 
 
 def solve(basis, fnc):
@@ -39,18 +40,16 @@ def solve(basis, fnc):
 
 data = getRectangularGridWithVectors(10, 10, 10)
 test_cell = data[5, 5, 5].vectors
-
-print(f"{data[5, 5, 5].x}, {data[5, 5, 5].y}, {data[5, 5, 5].z}")
+pdb.set_trace()
+print(test_cell)
+print()
+#print(f"{data[5, 5, 5].x}, {data[5, 5, 5].y}, {data[5, 5, 5].z}")
 print()
 a = fetch_vector(test_cell)
 #print(a)
 print()
-b = basis(a[0], data[5, 5, 5], a[1])
+b = basis(a[0])
 
-print(b)
 
-a = solve(b[0], b[1])
-print()
-print()
-print(a)
+
 
