e09e01cccd200dbd852e439cbc39fb6254a1d049
stefanrooze <prullemand@hotmail.com>
2021-04-28 15:06:18 +0200

    Changed the grid structure of the plots

--- Stefan/class_def.py
+++ Stefan/class_def.py
@@ -81,8 +81,10 @@ class gridBin:
             self.averageU = sumU / nrVectors
             self.averageV = sumV / nrVectors
             self.averageW = sumW / nrVectors
-        self.averageNormal = [self.averageU, self.averageV, self.averageW]
-        self.normalAverage.append([self.averageU, self.averageV, self.averageW])
+            self.averageNormal = [self.averageU, self.averageV, self.averageW]
+            self.normalAverage.append([self.averageU, self.averageV, self.averageW])
+        else:
+            self.averageNormal = []
 
     def calculateStandardDeviation(self):
 
@@ -162,61 +164,3 @@ class gridBin:
         self.averageGauss = [self.gaussU, self.gaussV, self.gaussW]
         self.gaussianAverage.append([self.gaussU, self.gaussV, self.gaussW])
 
-    def calculateNewGaussianAverage(self,datavarU, datavarV, datavarW, bigdatavarU, bigdatavarV, bigdatavarW):
-        gaussSumU, gaussSumV, gaussSumW = 0, 0, 0
-        gaussianWeightedU, gaussianWeightedV, gaussianWeightedW = 0, 0, 0
-
-        if np.size(self.vectors) > 1:
-            # use numpy to calculate the mean and standard deviation
-            for vector in self.vectors:
-                # calculate for every velocity component the normal value and sum this together for the bin
-                weightU = self.weightU
-                weightU2 = (1 / (np.sqrt(bigdatavarU) * np.sqrt(2 * np.pi))) * np.exp(-((vector.u - self.averageU) ** 2) / (2 * bigdatavarU))
-                weightUtot = weightU2 - weightU
-
-                # multiply the gaussian weight with the velocity component and sum all in the bin
-                gaussianWeightedU += weightUtot * vector.u
-                # sum all gaussian weights within the bin
-                gaussSumU += weightUtot
-
-                # calculate for every velocity component the normal value and sum this together for the bin
-                weightV = self.weightV
-                weightV2 = (1 / (np.sqrt(bigdatavarV) * np.sqrt(2 * np.pi))) * np.exp(-((vector.v - self.averageV) ** 2) / (2 * bigdatavarV))
-                weightVtot = weightV2 - weightV
-
-                # multiply the gaussian weight with the velocity component and sum all in the bin
-                gaussianWeightedV += weightVtot * vector.v
-                # sum all gaussian weights within the bin
-                gaussSumV += weightVtot
-
-                # calculate for every velocity component the normal value and sum this together for the bin
-                weightW = self.weightW
-                weightW2 = (1 / (np.sqrt(bigdatavarW) * np.sqrt(2 * np.pi))) * np.exp(-((vector.w - self.averageW) ** 2) / (2 * bigdatavarW))
-                weightWtot = weightW2 - weightW
-
-                # multiply the gaussian weight with the velocity component and sum all in the bin
-                gaussianWeightedW += weightWtot * vector.w
-                # sum all gaussian weights within the bin
-                gaussSumW += weightWtot
-
-            # devide the sum of the normal*velocity by the sum of the normal values to get the average gaussian velocity
-            self.newgaussU = gaussianWeightedU / (gaussSumU)
-            self.newgaussV = gaussianWeightedV / (gaussSumV)
-            self.newgaussW = gaussianWeightedW / (gaussSumW)
-
-        elif np.size(self.vectors) == 1:
-            # there is only one vector so that would mean standard deviation = 0 and Gaussian
-            # method can not be appolied so the average velocity is the velocity component of the particle
-            for vector in self.vectors:
-                self.newgaussU = vector.u
-                self.newgaussV = vector.v
-                self.newgaussW = vector.w
-
-        elif np.size(self.vectors) == 0:
-            # for an empty bin the velocity component is just 0
-            self.newgaussU = 0
-            self.newgaussV = 0
-            self.newgaussW = 0
-
-        self.averageNewGauss =[self.newgaussU,self.newgaussV,self.newgaussW]
-        self.newGaussianAverage.append([self.newgaussU, self.newgaussV, self.newgaussW])
