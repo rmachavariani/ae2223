454b76ccf8e0b513c3530d1e443b3df8c7bd0a73
clockserious <69359663+clockserious@users.noreply.github.com>
2021-04-30 00:14:41 +0200

    Formatted the turb plots

--- Maarten/TurbPlot.py
+++ Maarten/TurbPlot.py
@@ -11,11 +11,11 @@ matplotlib.use("Qt5Agg")
 # parameters
 nrOfParticles = None
 
-pitch = [30]#[10,15,20]
-radius = [30]#[10,15,20]
+pitch = [30]    # [10,15,20]
+radius = [30]   # [10,15,20]
 
 # load the grids
-grids = gr.allgrid(pitch,radius,nrOfParticles)
+grids = gr.allgrid(pitch, radius, nrOfParticles)
 
 
 t1 = time.time()
@@ -24,25 +24,24 @@ t1 = time.time()
 for grid in grids:
 
     # loop over current grid
-    for i in range(np.size(grid,axis=0)):
-        for j in range(np.size(grid,axis=1)):
-            for k in range(np.size(grid,axis=2)):
+    for i in range(np.size(grid, axis=0)):
+        for j in range(np.size(grid, axis=1)):
+            for k in range(np.size(grid, axis=2)):
 
                 # current bin
-                thisBin = grid[i,j,k]
+                thisBin = grid[i, j, k]
 
                 # calculate normal average
-                #thisBin.calculateNormalAverage()
+                # thisBin.calculateNormalAverage()
 
                 # calculate variance and deviation
-                #thisBin.calculateStandardDeviation()
-                #thisBin.calculateVariance()
+                # thisBin.calculateStandardDeviation()
+                # thisBin.calculateVariance()
 
                 # calculate gaussian average
-                #thisBin.calculateGaussianAverage()
+                # thisBin.calculateGaussianAverage()
 
                 # calculate dog gaussian average
-
 
                 # calculate poly fit
                 if len(thisBin.vectors) > 50:
@@ -51,28 +50,30 @@ for grid in grids:
 t2 = time.time()
 
 print()
-print("Total time for taking averages: ", round(t2-t1,2))
+print("Total time for taking averages: ", round(t2-t1, 2))
 print()
 print("----------------------------------------")
 
 
-def plotting(plane,location, locstr, number):
+def plotting(plane, location, locstr, number):
     t1 = time.time()
     for grid in grids:
         t_values= []
-        x_values, y_values, z_values = [],[],[]
+        x_values, y_values, z_values = [], [], []
 
-        levels = np.linspace(0, 40, number)
+        levels_1 = np.linspace(0, 35, number)
+        levels_2 = np.linspace(0, 25, number-2)
+        levels_3 = np.linspace(0, 30, number-1)
 
         if plane == "yz":
-            for i in range(np.size(grid,axis=0)):
-                if grid[i,0,0].x >=location:
+            for i in range(np.size(grid, axis=0)):
+                if grid[i, 0, 0].x >= location:
                     a = i
                     break
             print(grid[a,0,0].x)
-            for i in range(np.size(grid,axis=1)):
-                for j in range(np.size(grid,axis=2)):
-                    thisBin = grid[a,i,j]
+            for i in range(np.size(grid, axis=1)):
+                for j in range(np.size(grid, axis=2)):
+                    thisBin = grid[a, i, j]
                     x_values.append(thisBin.x)
                     y_values.append(thisBin.y)
                     z_values.append(thisBin.z)
@@ -82,14 +83,14 @@ def plotting(plane,location, locstr, number):
                     else:
                         t_values.append(0)
         elif plane == "xz":
-            for i in range(np.size(grid,axis=1)):
-                if grid[0,i,0].y >=location:
+            for i in range(np.size(grid, axis=1)):
+                if grid[0, i, 0].y >= location:
                     a = i
                     break
-            print(grid[0,a,0].y)
-            for i in range(np.size(grid,axis=0)):
-                for j in range(np.size(grid,axis=2)):
-                    thisBin = grid[i,a,j]
+            print(grid[0, a, 0].y)
+            for i in range(np.size(grid, axis=0)):
+                for j in range(np.size(grid, axis=2)):
+                    thisBin = grid[i, a, j]
                     x_values.append(thisBin.x)
                     y_values.append(thisBin.y)
                     z_values.append(thisBin.z)
@@ -99,14 +100,14 @@ def plotting(plane,location, locstr, number):
                     else:
                         t_values.append(0)
         elif plane == "xy":
-            for i in range(np.size(grid,axis=2)):
-                if grid[0,0,i].z >=location:
+            for i in range(np.size(grid, axis=2)):
+                if grid[0,0,i].z >= location:
                     a = i
                     break
             print(grid[0,0,a].z)
-            for i in range(np.size(grid,axis=0)):
-                for j in range(np.size(grid,axis=1)):
-                    thisBin = grid[i,j,a]
+            for i in range(np.size(grid, axis=0)):
+                for j in range(np.size(grid, axis=1)):
+                    thisBin = grid[i, j, a]
                     x_values.append(thisBin.x)
                     y_values.append(thisBin.y)
                     z_values.append(thisBin.z)
@@ -117,33 +118,33 @@ def plotting(plane,location, locstr, number):
                         t_values.append(0)
 
         if plane == "yz":
-            b_lst = np.array(y_values).reshape(np.size(grid,axis=1),np.size(grid,axis=2))
-            c_lst = np.array(z_values).reshape(np.size(grid,axis=1),np.size(grid,axis=2))
-            e_lst = np.array(t_values).reshape(np.size(grid,axis=1),np.size(grid,axis=2))
-            plt.contourf(b_lst, c_lst, e_lst, 100, levels=levels, cmap=cm.jet)
+            b_lst = np.array(y_values).reshape(np.size(grid, axis=1), np.size(grid, axis=2))
+            c_lst = np.array(z_values).reshape(np.size(grid, axis=1), np.size(grid, axis=2))
+            e_lst = np.array(t_values).reshape(np.size(grid, axis=1), np.size(grid, axis=2))
+            plt.contourf(b_lst, c_lst, e_lst, 100, levels=levels_1, cmap=cm.jet)
         elif plane == "xz":
-            a_lst = np.array(x_values).reshape(np.size(grid,axis=0),np.size(grid,axis=2))
-            c_lst = np.array(z_values).reshape(np.size(grid,axis=0),np.size(grid,axis=2))
-            e_lst = np.array(t_values).reshape(np.size(grid,axis=0),np.size(grid,axis=2))
-            plt.contourf(a_lst, c_lst, e_lst, 100, levels=levels, cmap=cm.jet)
+            a_lst = np.array(x_values).reshape(np.size(grid, axis=0), np.size(grid, axis=2))
+            c_lst = np.array(z_values).reshape(np.size(grid, axis=0), np.size(grid, axis=2))
+            e_lst = np.array(t_values).reshape(np.size(grid, axis=0), np.size(grid, axis=2))
+            plt.contourf(a_lst, c_lst, e_lst, 100, levels=levels_2, cmap=cm.jet)
         elif plane == "xy":
-            a_lst = np.array(x_values).reshape(np.size(grid,axis=0),np.size(grid,axis=1))
-            b_lst = np.array(y_values).reshape(np.size(grid,axis=0),np.size(grid,axis=1))
-            e_lst = np.array(t_values).reshape(np.size(grid,axis=0),np.size(grid,axis=1))
-            plt.contourf(a_lst, b_lst, e_lst, 100, levels=levels, cmap=cm.jet)
+            a_lst = np.array(x_values).reshape(np.size(grid, axis=0), np.size(grid, axis=1))
+            b_lst = np.array(y_values).reshape(np.size(grid, axis=0), np.size(grid, axis=1))
+            e_lst = np.array(t_values).reshape(np.size(grid, axis=0), np.size(grid, axis=1))
+            plt.contourf(a_lst, b_lst, e_lst, 100, levels=levels_3, cmap=cm.jet)
 
         t2 = time.time()
-        print("Time to order values",round(t2-t1,3),"sec")
+        print("Time to order values", round(t2-t1, 3), "sec")
 
         cb = plt.colorbar()
         plt.axis("square")
         plt.grid()
-        cb.set_label("k [J/kg]")
-        plt.title("Turbulent Kinetic Energy "+ plane + " at " + locstr + str(location))
+        cb.set_label("k [J/kg]", fontsize=15)
+        plt.title("Turbulent Kinetic Energy " + plane + " at " + locstr + str(location), fontsize=20)
         plt.show()
 
 
-plotting("yz",0, " x = ", 9)
-plotting("xz",0, " y = ", 9)
-plotting("xy",0, " z = ", 9)
+plotting("yz", 0, " x = ", 8)
+plotting("xz", 0, " y = ", 8)
+plotting("xy", 0, " z = ", 8)
 
