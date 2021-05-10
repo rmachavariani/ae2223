e7bf721a8bfa8434311f46ec4ca82f6d1e8629cd
stefanrooze <prullemand@hotmail.com>
2021-05-10 18:28:08 +0200

    Fixed that the plotting issues

---
+++ Stefan/EnsemblePolyfit.py
@@ -0,0 +1,140 @@
+#from gridConstruction import *
+import pdb
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from functools import partial as part
+import statistics as sts
+matplotlib.use("Qt5Agg")
+def fetch_vector(bin):
+    test_vectors = bin.vectors
+    particles_val = np.empty((len(test_vectors), 6))
+
+    if test_vectors == [] or () or None:
+        print("No particle")
+    else:
+        for i in range(len(test_vectors)):
+            x = test_vectors[i].x
+            y = test_vectors[i].y
+            z = test_vectors[i].z
+            u = test_vectors[i].u
+            v = test_vectors[i].v
+            w = test_vectors[i].w
+
+            particles_val[i] = [x, y, z, u, v, w]
+    # print(particles_val[:, 3])
+    return particles_val
+
+
+def basis(particles_val, cell):
+    design_matrix = np.empty((len(particles_val), 10))
+    for i in range(len(particles_val)):
+        dx = (particles_val[i, 0] - cell.x) / 1000
+        dy = (particles_val[i, 1] - cell.y) / 1000
+        dz = (particles_val[i, 2] - cell.z) / 1000
+
+        design_matrix[i] = [1, dx, dy, dz, dx * dy, dx * dz, dy * dz, dx ** 2, dy ** 2, dz ** 2]
+
+    # print(design_matrix)
+    return design_matrix
+
+
+def coefficients(basis, fnc):
+    coeffs = np.linalg.lstsq(basis, fnc, rcond=None)[0]
+
+    return coeffs
+
+
+
+def createPolyFit(coefs):
+
+    # create partical function
+    partialFit = part(polyFit, coefs)
+
+    return partialFit
+
+
+def polyFit(coefs, dx, dy, dz):
+
+    # define basis
+    basis = np.array([1, dx, dy, dz, dx * dy, dx * dz, dy * dz, dx ** 2, dy ** 2, dz ** 2])
+
+    # calculate value
+    functionValue = np.sum(basis * coefs)
+
+    return functionValue
+
+
+def vorticity(ucoefs, vcoefs, wcoefs):
+    curl = np.empty(3)
+    curl[0] = wcoefs[2] - vcoefs[3]
+    curl[1] = ucoefs[3] - wcoefs[1]
+    curl[2] = vcoefs[1] - ucoefs[2]
+
+    return curl
+
+
+
+
+
+
+def solve(bin):
+    #print('SOLVE EXECUTED')
+    data = fetch_vector(bin)
+    design_matrix = basis(data, bin)
+    ucoefs = coefficients(design_matrix, data[:, 3])
+    vcoefs = coefficients(design_matrix, data[:, 4])
+    wcoefs = coefficients(design_matrix, data[:, 5])
+    inst_vel = data[:, 3:6]
+
+    avg_vel = [ucoefs[0], vcoefs[0], wcoefs[0]]
+    vel_prime = [sts.stdev(inst_vel[:, 0], avg_vel[0]), sts.stdev(inst_vel[:, 1], avg_vel[1]), sts.stdev(inst_vel[:, 2], avg_vel[2])]
+    energy = 0.5 * (vel_prime[0]**2 + vel_prime[1]**2 + vel_prime[2]**2)
+
+    bin.fluc = vel_prime
+    bin.turb_eng = energy
+    bin.fitU = createPolyFit(ucoefs)
+    bin.fitV = createPolyFit(vcoefs)
+    bin.fitW = createPolyFit(wcoefs)
+    #print(ucoefs[0], vcoefs[0], wcoefs[0])
+    bin.polyfitAverage = [ucoefs[0], vcoefs[0], wcoefs[0]]
+    #print('POLYFIT EXECUTED')
+    #bin.vorticity = vort.vorticity(grid, bin, limits)
+
+# data = getRectangularGridWithVectors(10, 10, 10)
+#
+# sizeX = np.size(data, axis=0)
+# sizeY = np.size(data, axis=1)
+# sizeZ = np.size(data, axis=2)
+
+# for i in range(sizeX):
+#     for j in range(sizeY):
+#         for k in range(sizeZ):
+#             cell = data[i,j,k]
+#             if len(cell.vectors) > 50:
+#                 solve(cell)
+#                 print('---------')
+#                 counter  = (str(i) + ',' + str(j) + ',' + str(k))
+#                 print('particles in bin: ' + str(len(cell.vectors)))
+#                 print('cell #: ' + counter)
+#                 print('particles in bin: ')
+#                 print('cell x: ' + str(cell.x) + ' cell y: ' + str(cell.y) + ' cell z: '+ str(cell.z))
+#                 print('u: ' + str(cell.polyfitAverage[0]) + ' v: ' + str(cell.polyfitAverage[1]) + ' w: ' +str(cell.polyfitAverage[2]))
+#                 u, v, w = cell.polyfitAverage[0], cell.polyfitAverage[1], cell.polyfitAverage[2]
+#                 if u > 15 or u < -5 or abs(v) > 5 or abs(w) > 5:
+#                     print(u, v, w)
+#                     print('outlier detected!')
+#
+#
+#
+#
+#                 print('---------')
+
+
+
+#print(test_cell.polyfitAverage)
+#print(test_cell.vorticity)
+#print(test_cell.fluc)
+#print(test_cell.turb_eng)
+
+
---
+++ Stefan/vorticity.py
@@ -0,0 +1,141 @@
+import class_def
+import numpy as np
+import gridConstructionSphereFast as gr
+#from gridConstruction import *
+import EnsemblePolyfit as ens
+
+def get_nodes_array(location, scheme, direction):
+    #print(location, scheme, direction)
+    scheme_dict_shift = {'forward': 0, 'central': -2, 'backward': -4}
+
+    shift = np.zeros((5, 3))
+    for i in range(np.size(shift, axis=0)):
+        for j in range(np.size(shift, axis=1)):
+            if j == direction:
+                shift[i, j] = i + scheme_dict_shift[scheme]
+    #print(shift)
+    for i in range(np.size(shift, axis=0)):
+        shift[i,:] += location
+    #print(shift)
+    return shift.astype(int)
+
+def partial_approx(grid, scheme, veldir, griddir,poss,h, noData):
+
+    scheme_weights = {'forward': np.array([-25,48,-36,16,-3]), 'central' : np.array([1,-8,0,8,-1]), 'backward' : np.array([3,-16,36,-48,25])}
+
+    nodes = get_nodes_array(poss, scheme, griddir)
+    #print(nodes)
+    weighted_node_sum = 0
+    for cntr in range(np.size(nodes, axis=0)):
+
+        i = nodes[cntr,0]
+        j = nodes[cntr,1]
+        k = nodes[cntr,2]
+
+
+        node_bin = grid[i,j,k]
+        if not node_bin.polyfitAverage:
+            noData = True
+
+        else:
+            node_val = node_bin.polyfitAverage[veldir]
+            node_weight = scheme_weights[scheme][cntr]
+            weighted_node_sum += node_val * node_weight
+
+        if not noData:
+            return weighted_node_sum / (12 * h), noData
+        else:
+            return 1, noData
+
+
+def vorticity(grid, bin, limits):
+    # determine step size [m]
+    h = (grid[0, 0, 0].x - grid[1, 0, 0].x) / 1000
+
+    # Initialize the curl definition
+    pdes = ['dwdy', 'dvdz', 'dudz', 'dwdx', 'dvdx', 'dudy']
+    veldir = [2, 1, 0, 2, 1, 0]     # velocity components in vorticity defintion ()
+    griddir = [0, 2, 2, 0, 0, 1]
+
+    # Get position of the bin
+    poss = np.array([bin.i, bin.j, bin.k])
+
+    idx = 0
+    scheme = None
+    noData = False
+
+    # Determine scheme for the partial derivative
+    for pd in griddir:
+
+        if poss[pd] < 2:
+            scheme = 'forward'
+        elif poss[pd] > (limits[pd] - 2):
+            scheme = 'backward'
+        else:
+            scheme = 'central'
+
+        pdes[idx], noData = partial_approx(grid, scheme, veldir[idx], pd, poss, h, noData)
+        idx += 1
+    if not noData:
+        return [pdes[0] - pdes[1], pdes[2] - pdes[3], pdes[4] - pdes[5]]
+    else:
+        return []
+
+
+# DEBUGGING:
+#testGrid = getRectangularGridWithVectors(5,5,5)
+
+#print(testGrid[5,5,5])
+#print(vorticity(testGrid,testGrid[3,2,3]))
+
+#test = get_nodes_array([5,5,5], 'central', 1)
+#print(test)
+
+# ===============================================================
+
+# parameters
+# nrOfParticles = None
+#
+# pitch = [18.081]    # [10,15,20]
+# radius = [13]   # [10,15,20]
+#
+# # load the grids
+# grids = gr.allgrid(pitch, radius, nrOfParticles)
+# print(grids)
+#
+# t1 = time.time()
+#
+# # loop over grids to calculate averages
+# for grid in grids:
+#
+#     # loop over current grid
+#     for i in range(np.size(grid, axis=0)):
+#         for j in range(np.size(grid, axis=1)):
+#             for k in range(np.size(grid, axis=2)):
+#
+#                 # current bin
+#                 thisBin = grid[i, j, k]
+#
+#                 # calculate normal average
+#                 # thisBin.calculateNormalAverage()
+#
+#                 # calculate variance and deviation
+#                 # thisBin.calculateStandardDeviation()
+#                 # thisBin.calculateVariance()
+#
+#                 # calculate gaussian average
+#                 # thisBin.calculateGaussianAverage()
+#
+#                 # calculate dog gaussian average
+#
+#                 # calculate poly fit
+#                 if len(thisBin.vectors) > 50:
+#                     ens.solve(thisBin)
+#                     thisBin.vorticity = vorticity(grid,thisBin)
+#
+# t2 = time.time()
+#
+# print()
+# print("Total time for taking averages: ", round(t2-t1, 2))
+# print()
+# print("----------------------------------------")
